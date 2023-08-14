ae62b34efe49c65b59d08d755140041baf1b05f8
CLeovison <cleovison@gmail.com>
2023-08-14 16:47:54 +0800

    Accordion Almost Done

--- script/accordion.js
+++ script/accordion.js
@@ -1,2 +1,11 @@
-const faqS = document.querySelectorAll('.faqs');
+let faq = document.querySelectorAll('.faqs .container-main');
 
+faq.forEach((accordion) =>{
+    accordion.onclick = () =>{
+        faq.forEach((content) =>{
+            content.classList.remove('active');
+        })
+        accordion.classList.add('active');
+    }
+  
+})
