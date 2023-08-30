16c21414d867150894241bfd0627c024065f9afd
CLeovison <cleovison@gmail.com>
2023-08-30 11:36:25 +0800

    Tabs

--- script/tabs.js
+++ script/tabs.js
@@ -1,4 +1,13 @@
-const containerTabs = document.querySelectorAll('.container-tabs');
-const mainContents = document.querySelectorAll('.main-contents');
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.tabTarget);
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('active');
+        })
+        target.classList.add('active');
+    })
+}) 
