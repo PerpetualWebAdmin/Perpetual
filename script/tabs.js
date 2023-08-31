3b2a6d7f09bbe1d8ef1e964bfc77947a5b575133
CLeovison <cleovison@gmail.com>
2023-08-31 12:59:42 +0800

    Fixed

--- script/tabs.js
+++ script/tabs.js
@@ -4,6 +4,10 @@ const tabContents = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', ()=>{
+        tabs.forEach( x => {
+            x.classList.remove('active')
+        })
+        tab.classList.add('active')
         const target = document.querySelector(tab.dataset.tabTarget);
         tabContents.forEach(tabContent =>{
             tabContent.classList.remove('active');
