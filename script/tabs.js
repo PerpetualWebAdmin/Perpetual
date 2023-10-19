67db7a4f36c602bc44950493d0c6aca77a412f72
Leovison <cleovison@gmail.com>
2023-10-19 15:52:28 +0800

    University Week

--- script/tabs.js
+++ script/tabs.js
@@ -1,7 +1,6 @@
 const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
 
-
 tabs.forEach(tab =>{
     tab.addEventListener('click', ()=>{
         tabs.forEach( x => {
