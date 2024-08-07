72fa5ba17db2691acc62aefb67bc3a366c698526
Leovison <cleovison@gmail.com>
2024-08-07 15:28:17 +0800

    Added trial.php

--- admin/panel.js
+++ admin/panel.js
@@ -1,14 +1,30 @@
 const form = document.querySelector("form");
+const input = document.querySelector(".title");
+const textarea = document.querySelector("textarea");
+
+function AddAnnouncement() {
+    const newTitle = input.value
+    const newDescription = textarea.value
+
+    const divElem = document.createElement('div')
+    divElem.classList.add('trial')
+
+    const h1Elem = document.createElement('h1')
+    h1Elem.classList.add('title')
+   
+
+   divElem.appendChild(h1Elem)
+   console.log(divElem)
+    
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    const div = document.createElement('div')
-    div.classList.add('trial')
-    console.log(div)
-   
-    const p = document.createElement('p')
-    p.classList.add('trys')
-    p.textContent = "Shit"
-
-    const appends = div.appendChild(p)
+    if(input == ''){
+        throw new Error('The Title Section is Empty')
+    }
+    if(textarea == ''){
+        throw new Error('The Description Section is Empty')
+    }
+  AddAnnouncement();
 });
